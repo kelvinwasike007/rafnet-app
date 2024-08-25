f83480256130bb8e3b5f6bc67a57a075fd655fa0
kelvinwasike007 <kelvinwasike007@gmail.com>
2024-08-25 12:43:16 +0300

    capacitrot

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.rafnet.app',
+  appName: 'rafnet',
+  webDir: 'mobile/out',
+  bundledWebRuntime: false
+};
+
+export default config;
